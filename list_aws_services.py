93853e71c81e32c0125d25ef9f9094f55de9e7a4
orelyo <yorgen.orel@gmail.com>
2021-03-11 13:36:53 +0200

    Delete list_aws_services.py

--- list_aws_services.py
+++
@@ -1,5 +0,0 @@
-for region in aws ec2 describe-regions --output text | cut -f4;
-  do 
-     echo -e "\nListing Instances in region:'$region'...";
-      aws ec2 describe-instances --region $region | jq '.Reservations[] | ( .Instances[] | {state: .State.Name, name: .KeyName, type: .InstanceType, key: .KeyName})' > all_instances ;
-done
